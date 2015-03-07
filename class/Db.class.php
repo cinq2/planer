3fdab7b6245869e7b0d93041638da657dc596198
szaybus <szaybus@gmail.com>
2015-03-07 18:26:24 +0100

    dbclass

--- class/Db.class.php
+++ class/Db.class.php
@@ -1,8 +1,19 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "123", "tebedu");
+$mysqli = new mysqli("localhost", "Patryk", "123", "tebedu");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 echo $mysqli->host_info . "\n";
+
+
+class Db {
+	private $dbLogin = "Patryk";
+	private $dbPasswd = "123";
+	private $dbName = "tebedu";
+
+	public function __construct ()
+
+}
+
 
 ?>
