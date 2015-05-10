a425760847de75b9d02d64b031b4f6c1efc17ec5
szaybus <szaybus@gmail.com>
2015-05-10 10:42:14 +0200

    recommit

--- class/Db.class.php
+++ class/Db.class.php
@@ -4,16 +4,18 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 echo $mysqli->host_info . "\n";
-
+/*
 
 class Db {
 	private $dbLogin = "Patryk";
 	private $dbPasswd = "123";
 	private $dbName = "tebedu";
 
-	public function __construct ()
+	public function __construct () {
+	return $this->dbPasswd;
+	}
 
 }
-
+*/
 
 ?>
--- class/teacher.php
+++ class/teacher.php
@@ -22,12 +22,31 @@ private $db;
    }
    function get($_id) {
       $q = "SELECT * FROM Nauczyciel WHERE ID_Nauczyciela='".$_id."';";
+      
      return $this->db->query($q);
    }
    function getall() {
-      
       $q = "SELECT * FROM Nauczyciel";
+      echo $q;
    return $this->db->query($q);
+   }
+   function getHTMLtable() {
+		$q = "SELECT * FROM Nauczyciel";
+		$result = $this->db->query($q);
+		if ($result->num_rows > 0) {
+		// output data of each row
+		echo "<table>";
+		while($row = $result->fetch_assoc()) {
+		echo "<tr>";
+        echo '<td>'.$row["Id_Nauczyciela"].'</td><td>'.$row["Imie"].'</td><td>'.$row["Nazwisko"].'</td>';
+        echo '<td><a href="nauczyciel.php?tryb=edytuj&id='.$row["Id_Nauczyciela"].'">Edytuj</a></td>';
+        echo '<td><a href="nauczyciel.php?tryb=usun&id='.$row["Id_Nauczyciela"].'">Usuń</a></td>';
+        echo "</tr>";
+		}
+		echo "</table>";
+		}	 else {
+			echo "0 results";
+		}
    }
    function remove($_id) {
       $q = "DELETE FROM nauczyciel WHERE ID_Nauczyciela='".$_id."';";
